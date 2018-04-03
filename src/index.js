9d52bd41ff3aa888f7f76cc3607522d22cb7b2f2
andy <myxbing@hotmail.com>
2018-04-03 15:52:29 +0800

    配置脚本开发。

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-alert('test');
+import $ from 'jquery';
+$(function() {
+    alert('test');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,53 @@
 const webpack = require('webpack');
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-exports.module = {
-    entry: {
-        app: [path.join(__dirname, 'src', 'index.js')]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 8080,
-        host: '0.0.0.0',
-        historyApiFallback: true,
-        hot: true
-    }
+module.exports = (env) => {
+    const isDev = !(env && env.prod);
+    return [{
+        entry: path.join(__dirname, 'src', 'index.js'),
+        output: {
+            filename: 'bundle.js',
+            path: path.join(__dirname, 'dist')
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                include: path.join(__dirname, "src"),
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }, {
+                test: /\.html$/,
+                include: path.join(__dirname, "src"),
+                loader: 'file-loader'
+            }]
+        },
+        plugins: (isDev ? [
+            new webpack.HotModuleReplacementPlugin()
+        ] : [new UglifyJsPlugin({
+            uglifyOptions: {
+                ecma: 6 //es6支持
+            }
+        })]).concat([
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery"
+            }),
+            new webpack.optimize.OccurrenceOrderPlugin(true),
+            new webpack.IgnorePlugin(/jquery/)
+        ]),
+        devServer: {
+            contentBase: path.join(__dirname, "dist"),
+            port: 8080,
+            host: '0.0.0.0',
+            historyApiFallback: true,
+            hot: true
+        }
+    }];
 };
