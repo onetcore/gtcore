5e279e405e62f1e8dcd66b51a02b400641b45137
小兵 <xiaobing@xiaobing.local>
2018-11-08 01:08:30 +0800

    菜单。

--- mozlite/src/markdown.js
+++ mozlite/src/markdown.js
@@ -1,51 +1,76 @@
-import {queue} from './core';
-import {markdown} from 'markdown';
+import {
+    queue
+} from './core';
+import {
+    markdown
+} from 'markdown';
 
-class MozMD{
-    constructor(selector){
+class MozMD {
+    constructor(selector) {
         this.selector = selector;
         this.preview = $('.mozmd-preview', selector);
         this.source = $('.mozmd-source', selector);
         this.actions = $('.mozmd-toolbar a', selector);
+        this._value = $('.mozmd-source-value', selector);
+        this._html = $('.mozmd-html-value', selector);
         this.init();
     }
 
-    init(){
-        this.source.on('input', function(){
-            var source = this.innerText;
-            _preview.html(markdown.toHTML(source));
-            this.selector.trigger('mozmd.input');
-        });
-        this.actions.exec(current=>current.off('click').on('click', e=>{
-            var _this = $(e.target);
-            switch(_this.attr('class')){
+    get value(){
+        return this._value.val();
+    }
+
+    set value(source){
+        this.source.text(source);
+        this.update();
+    }
+
+    get html(){
+        return this._html.val();
+    }
+
+    update() {
+        var source = this.source.text();
+        this._value.val(source);
+        var html = markdown.toHTML(source);
+        this.preview.html(html);
+        this._html.val(html);
+        this.selector.trigger('mozmd.updated');
+    }
+
+    init() {
+        this.source.on('input', e => this.update());
+        this.actions.exec(current => current.off('click').on('click', e => {
+            var name = current.attr('class');
+            switch (name) {
                 case 'mozmd-fullscreen':
                     $(document.body).addClass('fullscreen');
                     this.selector.addClass('fullscreen-container');
-                    _this.attr('class', 'mozmd-exitfull').attr('title','退出全屏').find('i').attr('class', 'fa fa-window-restore');
-                break;
+                    current.attr('class', 'mozmd-exitfull').attr('title', '退出全屏').find('i').attr('class', 'fa fa-window-restore');
+                    break;
                 case 'mozmd-exitfull':
                     $(document.body).removeClass('fullscreen');
                     this.selector.removeClass('fullscreen-container');
-                    _this.attr('class', 'mozmd-fullscreen').attr('title','全屏显示').find('i').attr('class', 'fa fa-window-maximize');
-                break;
+                    current.attr('class', 'mozmd-fullscreen').attr('title', '全屏显示').find('i').attr('class', 'fa fa-window-maximize');
+                    break;
                 case 'mozmd-mode-preview':
-                    _source.hide();
-                    _preview.show();
-                    _this.attr('class', 'mozmd-mode-source').attr('title','源码').find('i').attr('class', 'fa fa-code');
-                break;
+                    this.source.hide();
+                    this.preview.show();
+                    current.attr('class', 'mozmd-mode-source').attr('title', '源码').find('i').attr('class', 'fa fa-code');
+                    break;
                 case 'mozmd-mode-source':
-                    _source.show();
-                    _preview.hide();
-                    _this.attr('class', 'mozmd-mode-preview').attr('title','预览').find('i').attr('class', 'fa fa-eye');
-                break;
+                    this.source.show();
+                    this.preview.hide();
+                    current.attr('class', 'mozmd-mode-preview').attr('title', '预览').find('i').attr('class', 'fa fa-eye');
+                    break;
             }
+            this.selector.trigger(name);
         }));
     }
 }
 
-queue(context=>{
-    $('.mozmd-editor', context).exec(current=>{
-       current.data('mozmd-editor', new MozMD(current));
+queue(context => {
+    $('.mozmd-editor', context).exec(current => {
+        current.data('mozmd-editor', new MozMD(current));
     });
 });
